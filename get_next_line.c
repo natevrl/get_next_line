f69eace1a9e61c42d7baa98ffc405964434d78d7
v3r <v3rslor@protonmail.com>
2021-12-02 21:46:10 +0100

    mon get next line fonctione :)) mais rien nest proteger, encore du taff

--- get_next_line.c
+++ get_next_line.c
@@ -15,104 +15,62 @@ int	ftstrlen(char *str)
 	return (i);
 }
 
-/* idee fonctions : 
- * read_one_buffer : contients le read() qui lit le buffer et lenvoie a new_line_tchecker
- * new_line_checker : tcheck tout les caracteres du buffer pour trouver une newline
- * deux possibilites :
- * ====================> 1 : le buffer contiens une newline
- * --> memoriser lindex de cette newline et le renvoyer, 
- * --> malloc lindex + 1 pour '\0'
- * --> get_next_line renvoie la ligne
- * -->
- * ====================> 2 : le buffer ne contient pas de newline
- * */
-
-// fonction qui check si il y a un '\n' dans le buffer
-int new_line_checker(char *str)
-{
-	int i;
-
-	i = -1;
-	while (str[++i])
-		if (str[i] == '\n')
-			return (i);
-	return (0);
-}
-
 char *join_all(int fd)
 {
 	char buffer[BUFFER_SIZE];
-	char *tmp;
-	char *str;
-	int i = 0;
+	static char *tmp;
 	int read_return;
 
-	tmp = "";
+	if (!tmp)
+		tmp = "";
 	while ((read_return = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[read_return] = '\0';
-		printf("%c\n", buffer[read_return - 1]);
-		//printf("%d\n", read_return);
 		tmp = ft_strjoin(tmp, buffer);
 	}
-	//printf("%c\n", tmp[0]);
-	str = malloc(sizeof(char) * ft_strlen(tmp) + 1);
-	if (!str)
-	{
-		printf("alloc echouer ptain\n");
-		return (NULL);
-	}
-	while (tmp[i])
-	{
-		str[i] = tmp[i];
-		i++;
-	}
-	str[i] = '\0';
-	free(tmp);
-	return (str);
+	return (tmp);
 }
 
 char *get_next_line(int fd)
 {
 	char	*the_line;
 	char *str;
-	int i = 0;
+	static int i = 0;
 	int y = 0;
-
-	y += i;
+	
 	str = join_all(fd);
-	//printf("str ; %s\n", str);
-	while (str[i] != '\n' && str[i] != '\0')
-	{
+	if (str[i] == '\n')
 		i++;
-		printf("ca rentre");
-	}
+	y = i;
+	while (str[i] != '\n' && str[i])
+		i++;
 
-	printf("i = %d\n", i);
-	the_line = ft_substr(str, y, i);
-	i++;
-	//free(str);
+	the_line = ft_substr(str, y, i - y + 1);
+	printf("%d\n", i);
 	return (the_line);
 }
 
-int test()
+char *test(char *str)
 {
+	char *new;
 	static int i = 0;
-	char str[] = "test1test1test1qqq";
-	while (str[i] != '1' && str[i])
+	int y = 0;
+
+	if (str[i] == '\n')
 		i++;
-	i++;
-	printf("test111");
-	return i;
+	y = i;
+	while (str[i] != '\n' && str[i])
+		i++;
+	new = ft_substr(str, y, i - y + 1);
+	printf("%d\n", i);
+	return new;
 }
 
+#include <stdio.h>
 int main()
 {
 	int fd;
-	char *str;
-	//char *gnl;
-	//char *gnl2;
-	//char *gnl3;
+	//char str[] = "qwer\ntyyy\nxxxx";
 
 	fd = open("test.txt", O_RDONLY);
 	if (fd == -1)
@@ -120,10 +78,15 @@ int main()
 		ft_putstr_fd("open() error", 1);
 		return (0);
 	}
-	str = join_all(fd);
-	printf("%s", str);
-	free(str);
-	//printf("%d\n", test());
+	//printf("%s", test(str));	
+	//printf("%s", test(str));	
+	//printf("%s", test(str));	
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	//free(get_next_line(fd));
+	//printf("%s\n", join_all(fd));
 	
 	return (0);
 }
+
