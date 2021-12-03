2c80d1b4e69b2875678063ea5d526f137dba8230
Nathan BENHADOUCHE <nbenhado@e3r3p21.clusters.42paris.fr>
2021-12-03 19:16:20 +0100

    resolution de cas speciaux

--- get_next_line.c
+++ get_next_line.c
@@ -3,7 +3,27 @@
 #include "../42-libft/libft.h"
 #include <stdlib.h>
 
-# define BUFFER_SIZE 1
+# define BUFFER_SIZE 10
+
+int	ft_strcmp(char	*s1, char	*s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+			break ;
+	}
+	if (*s1 > *s2)
+		return (1);
+	else if (*s2 == *s1)
+		return (0);
+	else
+		return (-1);
+}
 
 int	ftstrlen(char *str)
 {
@@ -14,7 +34,6 @@ int	ftstrlen(char *str)
 		i++;
 	return (i);
 }
-
 
 // fonction qui check si il y a un '\n' dans le buffer
 int there_is_newline(char *str)
@@ -28,9 +47,7 @@ int there_is_newline(char *str)
 	return (0);
 }
 
-//int stock_after_newline()
-
-char *join_all(int fd)
+char *get_next_line(int fd)
 {
 	char buffer[BUFFER_SIZE];
 	char *new_line;
@@ -38,6 +55,10 @@ char *join_all(int fd)
 	static char *stock_after_newline = NULL;
 	int read_return;
 
+	if (BUFFER_SIZE <= 0)
+		return (NULL);
+	if (stock_after_newline[0] == "")
+		return (NULL);
 	if (stock_after_newline != NULL)
 		tmp = stock_after_newline;
 	else
@@ -47,48 +68,18 @@ char *join_all(int fd)
 		buffer[read_return] = '\0';
 		tmp = ft_strjoin(tmp, buffer);
 		if (there_is_newline(tmp))
-		{
 			break ;
-		}
 	}
-	//								STR		DEBUT 					SIZE TOTAL A COPIER
-	stock_after_newline = ft_substr(tmp, there_is_newline(tmp) + 1, ftstrlen(tmp) - (there_is_newline(tmp) + 1));
-	new_line = ft_substr(tmp, 0, there_is_newline(tmp) + 1);
+	if (there_is_newline(tmp))
+	{
+		//								STR		DEBUT 					SIZE TOTAL A COPIER
+		stock_after_newline = ft_substr(tmp, there_is_newline(tmp) + 1, ftstrlen(tmp) - (there_is_newline(tmp) + 1));
+		new_line = ft_substr(tmp, 0, there_is_newline(tmp) + 1);
+	}
+	else
+		new_line = ft_substr(tmp, 0, ftstrlen(tmp));
+	free(tmp);
 	return (new_line);
-}
-
-char *get_next_line(int fd)
-{
-	char	*the_line;
-	char *str;
-	static int i = 0;
-	int y = 0;
-	
-	str = join_all(fd);
-	if (str[i] == '\n')
-		i++;
-	y = i;
-	while (str[i] != '\n' && str[i])
-		i++;
-
-	the_line = ft_substr(str, y, i - y + 1);
-	return (the_line);
-}
-
-char *test(char *str)
-{
-	char *new;
-	static int i = 0;
-	int y = 0;
-
-	if (str[i] == '\n')
-		i++;
-	y = i;
-	while (str[i] != '\n' && str[i])
-		i++;
-	new = ft_substr(str, y, i - y + 1);
-	printf("%d\n", i);
-	return new;
 }
 
 #include <stdio.h>
@@ -103,12 +94,38 @@ int main()
 		ft_putstr_fd("open() error", 1);
 		return (0);
 	}
-	printf("%s", join_all(fd));
-	printf("%s", join_all(fd));
-	printf("%s", join_all(fd));
-	printf("%s", join_all(fd));
-	printf("%s", join_all(fd));
-	
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	//printf("%s", get_next_line(fd));
+	//printf("%s", get_next_line(fd));
 	return (0);
 }
+/*int    main()
+{
+    int        i;
+    int        fd;
+    char    *str;
 
+    i = 1;
+    fd = open("test.txt", O_RDONLY);
+    if (!fd)
+    {
+        printf("fd opening error\n");
+        return (-1);
+    }
+    str = get_next_line(fd);
+    while (str)
+    {
+        printf("Line %d = %s", i, str);
+        free(str);
+        str = get_next_line(fd);
+        i++;
+    }
+    free(str);
+    close(fd);
+    return (0);
+}*/
