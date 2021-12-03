b4c76f4a8fd23f38429d6379a7979dedabc64bbd
Nathan BENHADOUCHE <nbenhado@e3r3p21.clusters.42paris.fr>
2021-12-03 16:55:38 +0100

    nouvelle version faite, la base fonctionne

--- get_next_line.c
+++ get_next_line.c
@@ -3,7 +3,7 @@
 #include "../42-libft/libft.h"
 #include <stdlib.h>
 
-# define BUFFER_SIZE 10000000
+# define BUFFER_SIZE 1
 
 int	ftstrlen(char *str)
 {
@@ -15,20 +15,46 @@ int	ftstrlen(char *str)
 	return (i);
 }
 
+
+// fonction qui check si il y a un '\n' dans le buffer
+int there_is_newline(char *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i])
+		if (str[i] == '\n')
+			return (i);
+	return (0);
+}
+
+//int stock_after_newline()
+
 char *join_all(int fd)
 {
 	char buffer[BUFFER_SIZE];
-	static char *tmp;
+	char *new_line;
+	char *tmp;
+	static char *stock_after_newline = NULL;
 	int read_return;
 
-	if (!tmp)
+	if (stock_after_newline != NULL)
+		tmp = stock_after_newline;
+	else
 		tmp = "";
 	while ((read_return = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[read_return] = '\0';
 		tmp = ft_strjoin(tmp, buffer);
+		if (there_is_newline(tmp))
+		{
+			break ;
+		}
 	}
-	return (tmp);
+	//								STR		DEBUT 					SIZE TOTAL A COPIER
+	stock_after_newline = ft_substr(tmp, there_is_newline(tmp) + 1, ftstrlen(tmp) - (there_is_newline(tmp) + 1));
+	new_line = ft_substr(tmp, 0, there_is_newline(tmp) + 1);
+	return (new_line);
 }
 
 char *get_next_line(int fd)
@@ -77,16 +103,11 @@ int main()
 		ft_putstr_fd("open() error", 1);
 		return (0);
 	}
-	//printf("%s", test(str));	
-	//printf("%s", test(str));	
-	//printf("%s", test(str));	
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	//free(get_next_line(fd));
-	//printf("%s\n", join_all(fd));
+	printf("%s", join_all(fd));
+	printf("%s", join_all(fd));
+	printf("%s", join_all(fd));
+	printf("%s", join_all(fd));
+	printf("%s", join_all(fd));
 	
 	return (0);
 }
